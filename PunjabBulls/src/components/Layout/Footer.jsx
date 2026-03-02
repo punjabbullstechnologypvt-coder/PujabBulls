8ce51bcc7a2db779305bb68b010f7d27884e2b10
Pritha Rai <pritharai873@gmail.com>
2026-03-02 21:30:41 +0530

    Footer social link added

--- PunjabBulls/src/components/Layout/Footer.jsx
+++ PunjabBulls/src/components/Layout/Footer.jsx
@@ -91,7 +91,7 @@ const Footer = () => {
             </p>
 
             <div className="flex gap-4">
-              <a className="text-gray-400 hover:text-white" href="#">
+              <a className="text-gray-400 hover:text-white" href="https://www.linkedin.com/company/punjabbullstechnologypvtltd-relax-erp/posts/?feedView=all">
                 <span className="sr-only">LinkedIn</span>
                 <svg
                   className="h-5 w-5"
@@ -102,7 +102,7 @@ const Footer = () => {
                 </svg>
               </a>
 
-              <a className="text-gray-400 hover:text-white" href="#">
+              {/* <a className="text-gray-400 hover:text-white" href="#">
                 <span className="sr-only">Twitter</span>
                 <svg
                   className="h-5 w-5"
@@ -111,7 +111,7 @@ const Footer = () => {
                 >
                   <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
                 </svg>
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
