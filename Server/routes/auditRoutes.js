a7444484b8ed1263b17bce4e864d1cc3652cf7be
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2026-03-17 19:12:12 +0530

    Added a comment for server redeploy

--- Server/routes/auditRoutes.js
+++ Server/routes/auditRoutes.js
@@ -3,7 +3,7 @@ import { protectAdmin } from "../middleware/authMiddleware.js";
 import { getImageAuditLogs } from "../controllers/auditController.js";
 
 const router = express.Router();
-
+// reploy commit
 router.get("/image-events", protectAdmin, getImageAuditLogs);
 
 export default router;
