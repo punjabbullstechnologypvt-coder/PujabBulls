b2cf2606c8941ca87e7f1c49591d0a31842071db
punjabbullstechnologypvt-coder <punjabbullstechnologypvt@gmail.com>
2026-07-18 11:28:10 +0530

    Update SplashScreen.jsx

--- PunjabBulls/src/components/SplashScreen.jsx
+++ PunjabBulls/src/components/SplashScreen.jsx
@@ -27,7 +27,7 @@ export default function SplashScreen({ onFinish }) {
       <div className="splash-content">
         <div className="logo-container">
           <img 
-            src="https://res.cloudinary.com/ducv9j3hj/image/upload/v1770456470/logo_ry6usn.png" 
+            src = "/images/pb-logo.png"
             alt="PunjabBulls" 
             className="splash-logo"
           />
